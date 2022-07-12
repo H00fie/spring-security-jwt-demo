13608dee5d18c754b705fa8a2934c0dbc324890d
Dazikiri Anikar <mierzwinski.b@gmail.com>
2022-07-12 19:09:55 +0200

    Implemented the initial JSON custom mapping.

---
+++ src/main/java/bm/app/springsecurityjwtdemo/config/Config.java
@@ -0,0 +1,24 @@
+package bm.app.springsecurityjwtdemo.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * To customize the process of mapping JSONs and objects to JSONs.
+ */
+@Configuration
+public class Config {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    /**
+     * For example, I can make my mapper not serialize null fields.
+     */
+    void customizeObjectMapper() {
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+}
