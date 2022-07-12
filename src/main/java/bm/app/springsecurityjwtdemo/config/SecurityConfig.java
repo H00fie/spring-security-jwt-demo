52ccb29b57916ecd901496d29df33e672e0fbc06
Dazikiri Anikar <mierzwinski.b@gmail.com>
2022-07-12 16:57:32 +0200

    Configured HTTP so Swagger and H2 console can be accessed while all other endpoints are secured.

--- src/main/java/bm/app/springsecurityjwtdemo/config/SecurityConfig.java
+++ src/main/java/bm/app/springsecurityjwtdemo/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package bm.app.springsecurityjwtdemo.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -25,5 +26,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //I need to specify how my password will be encoded. Using the "bcrypt" way, I am encoding "test".
                 .password("{bcrypt}" + new BCryptPasswordEncoder().encode("test"))
                 .roles("USER"); //The user's role.
+    }
+
+    /**
+     * HTTP configuration so I can access Swagger and H2 Console while all the other endpoints are secured.
+     * A 'csrf' (Cross-Site Request Forgery) is an attack that forces authenticated users to submit a request
+     * to a Web application against which they are currently authenticated.
+     * It's being disabled because it's not required if a stateless API uses token-based authentication.
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http
+                .authorizeRequests()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/v2/api-docs").permitAll()
+                .antMatchers("/webjars/**").permitAll()
+                .antMatchers("swagger-resources/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll();
     }
 }
