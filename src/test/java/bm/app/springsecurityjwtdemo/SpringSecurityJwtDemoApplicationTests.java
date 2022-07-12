dc06def2ca6e2a4157afa8ba93bc9b9dc9374e91
Dazikiri Anikar <mierzwinski.b@gmail.com>
2022-07-12 16:08:48 +0200

    The initial commit.

---
+++ src/test/java/bm/app/springsecurityjwtdemo/SpringSecurityJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bm.app.springsecurityjwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
