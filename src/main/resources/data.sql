033fc526949f48cb3e0b1778fa82b317cffd6d88
Bartosz76 <mierzwinski.b@gmail.com>
2022-07-13 21:36:20 +0200

    Added atable with comments in schema.sql file and added more test data to data.sql file.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,9 @@
-insert into post(id, title, content) values (1, 'test post', 'content 1');
+insert into post(id, title, content, created) values (1, 'test post 1', 'content 1', '2022-07-23T21:14:30.666666666');
+insert into post(id, title, content, created) values (2, 'test post 2', 'content 2', '2022-07-23T21:14:30.666666666');
+insert into post(id, title, content, created) values (3, 'test post 3', 'content 3', '2022-07-23T21:14:30.666666666');
+insert into comment(id, post_id, content, created) values (1, 1, 'comment 1', '2022-07-23T21:14:30.666666666');
+insert into comment(id, post_id, content, created) values (2, 1, 'comment 2', '2022-07-23T21:14:30.666666666');
+insert into comment(id, post_id, content, created) values (3, 2, 'comment 3', '2022-07-23T21:14:30.666666666');
+insert into comment(id, post_id, content, created) values (4, 2, 'comment 4', '2022-07-23T21:14:30.666666666');
+insert into comment(id, post_id, content, created) values (5, 3, 'comment 5', '2022-07-23T21:14:30.666666666');
+insert into comment(id, post_id, content, created) values (6, 3, 'comment 6', '2022-07-23T21:14:30.666666666');
